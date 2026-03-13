62ee1817e84c7f87ccac0b36d964c6fb92af4c69
yc <105861890+yanchn-lim@users.noreply.github.com>
2026-03-13 23:55:34 +0800

    fixed world transform propagation in scene

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -130,6 +130,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 			if (AEInputCheckTriggered(AEVK_3)) 
 				SceneManager::RequestSceneSwitch("Play_Level");	
+
+			if (AEInputCheckTriggered(AEVK_4))
+				SceneManager::RequestSceneSwitch("physics_test");
 		}
 
 		if (AEInputCheckTriggered(AEVK_F5))
